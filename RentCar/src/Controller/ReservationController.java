733ca552e2121ef64918199315a5a8da5ac2f7f5
SaajidNawwar <68622804+SaajidNawwar@users.noreply.github.com>
2020-10-24 11:27:35 +0530

    Update ReservationController.java

--- RentCar/src/Controller/ReservationController.java
+++ RentCar/src/Controller/ReservationController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.io.IOException;
+
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -16,17 +17,34 @@ import Entity.Reservation;
 
 @WebServlet("/ReservationController")
 public class ReservationController extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 
 	RequestDispatcher dispatcher = null;
 
-	// create a ref variable to reservation dao
+	// create a ref variable to reservation daa///////
 	ReservationDAO reservationdao = null;
 
 	// create constructor and initialze reservation dao
 	public ReservationController() {
+		
+		//create dao object
 		reservationdao = new ReservationDAOImpl();
 
+	}
+	
+	public void listReservation(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("controller");
+		// call dao method to get list of reservation
+		List<Reservation> list = reservationdao.get();
+
+		// add the reservations to request object
+		request.setAttribute("list", list);
+
+		dispatcher = request.getRequestDispatcher("/dashbord.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -41,97 +59,93 @@ public class ReservationController extends HttpServlet {
 		case "LIST":
 			listReservation(request, response);
 			break;
-			
+
 		case "EDIT":
-			getSingleReservation (request,response);
+			getSingleReservation(request, response);
 			break;
-			
+
 		case "DELETE":
-			deleteReservation (request,response);
+			deleteReservation(request, response);
 			break;
-			
+
 		default:
-			listReservation(request,response);
+			listReservation(request, response);
 			break;
-			
+
 		}
 
 	}
 
-	private void deleteReservation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private void deleteReservation(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String rid = request.getParameter("rid");
-		if(reservationdao.delete(Integer.parseInt(rid))) {
+		if (reservationdao.delete(Integer.parseInt(rid))) {
 			request.setAttribute("message", "Record has been deleted");
 		}
 		listReservation(request, response);
-		
+
 	}
 
-	private void getSingleReservation(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private void getSingleReservation(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String rid = request.getParameter("rid");
+
 		Reservation reservation = reservationdao.get(Integer.parseInt(rid));
-		
+
 		request.setAttribute("reservation", reservation);
-		 
-		 dispatcher = request.getRequestDispatcher("add-reservation.jsp");
-			dispatcher.forward(request, response);
-		
+
+		dispatcher = request.getRequestDispatcher("add-reservation.jsp");
+		dispatcher.forward(request, response);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String rid = request.getParameter("rid"); 
+		String rid = request.getParameter("rid");
 		String location = request.getParameter("location");
 		String time1 = request.getParameter("time1");
 		String pickupdate = request.getParameter("pickupdate");
 		String returndate = request.getParameter("returndate");
-		String numofdays = request.getParameter("numofdays");
+//		String numofdays = request.getParameter("numofdays");
 		String numofpassengers = request.getParameter("numofpassengers");
-		
-		
 
 		Reservation r = new Reservation();
-		
-		
+
 		r.setLocation(location);
 		r.setTime1(time1);
 		r.setPickupdate(pickupdate);
 		r.setReturndate(returndate);
-		r.setNumofdays(numofdays);
+//		r.setNumofdays(numofdays);
 		r.setNumofpassengers(numofpassengers);
-		
-		if(rid.isEmpty() || rid==null) {
-			if(reservationdao.save(r)) {
+
+		if (rid.isEmpty() || rid == null) {
+
+			if (reservationdao.save(r)) {
+
 				request.setAttribute("message", "Saved Successfully");
 			}
-		}else {
+		} else {
+
 			r.setRid(Integer.parseInt(rid));
+
 			if (reservationdao.update(r)) {
 				request.setAttribute("message", "updated Successfully");
 			}
+			dispatcher = request.getRequestDispatcher("/dashbord.jsp");
+			dispatcher.forward(request, response);
+			//listReservation(request, response);
+			
+			
 		}
 
-		
 		dispatcher = request.getRequestDispatcher("/Product.jsp");
 		dispatcher.forward(request, response);
 		listReservation(request, response);
 
 	}
 
-	public void listReservation(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
 
-		System.out.println("controller");
-		// call dao method to get list of reservation
-		List<Reservation> list = reservationdao.get();
-
-		// add the reservations to request object
-		request.setAttribute("list", list);
-
-		dispatcher = request.getRequestDispatcher("/dashbord.jsp");
-		dispatcher.forward(request, response);
-	}
 
 }
